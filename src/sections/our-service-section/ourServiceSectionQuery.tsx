d024122446d387ebfb46840571982b335e9645bf
RopaAscari <danyelroper@email.com>
2024-04-23 12:34:50 -0500

    updates

--- src/sections/our-service-section/ourServiceSectionQuery.tsx
+++ src/sections/our-service-section/ourServiceSectionQuery.tsx
@@ -13,56 +13,56 @@ export const ourServiceSectionQuery = graphql`
         landscapeImage: file(relativePath: {eq: "landscape_image.png"} ){
             childImageSharp {
                 fluid(maxWidth: 500){
-                    ...GatsbyImageSharpFluid_noBase64
+                    
                 }
             }
         }
         paintingImage: file(relativePath: {eq: "painting_image.png"} ){
             childImageSharp {
                 fluid(maxWidth: 500){
-                    ...GatsbyImageSharpFluid_noBase64
+                    
                 }
             }
         }
         plumbingImage: file(relativePath: {eq: "plumbing_image.png"} ){
             childImageSharp {
                 fluid(maxWidth: 500){
-                    ...GatsbyImageSharpFluid_noBase64
+                    
                 }
             }
         }
         weldingImage: file(relativePath: {eq: "welding_image.png"} ){
             childImageSharp {
                 fluid(maxWidth: 500){
-                    ...GatsbyImageSharpFluid_noBase64
+                    
                 }
             }
         }
         gardenImage: file(relativePath: {eq: "garden_image.png"} ){
             childImageSharp {
                 fluid(maxWidth: 500){
-                    ...GatsbyImageSharpFluid_noBase64
+                    
                 }
             }
         }
         electricalImage: file(relativePath: {eq: "electrical_image.png"} ){
             childImageSharp {
                 fluid(maxWidth: 500){
-                    ...GatsbyImageSharpFluid_noBase64
+                    
                 }
             }
         }
         closetImage: file(relativePath: {eq: "closet_image.png"} ){
             childImageSharp {
                 fluid(maxWidth: 500){
-                    ...GatsbyImageSharpFluid_noBase64
+                    
                 }
             }
         }
         applianceImage: file(relativePath: {eq: "appliance_image.png"} ){
             childImageSharp {
                 fluid(maxWidth: 500){
-                    ...GatsbyImageSharpFluid_noBase64
+                    
                 }
             }
         }
