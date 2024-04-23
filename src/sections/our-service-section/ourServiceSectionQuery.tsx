fc086c41ecfcec7347dff889be9c2b57b4a1a940
RopaAscari <danyelroper@email.com>
2024-04-23 13:25:04 -0500

    updates

--- src/sections/our-service-section/ourServiceSectionQuery.tsx
+++ src/sections/our-service-section/ourServiceSectionQuery.tsx
@@ -1,69 +1,86 @@
 import { graphql } from "gatsby";
 
 export const ourServiceSectionQuery = graphql`
-    query{
-        carpentryImage: file(relativePath: {eq: "carpentry_image.png"} ){
+    query {
+        carpentryImage: file(relativePath: {eq: "carpentry_image.png"}){
             childImageSharp {
                 gatsbyImageData(
+                    width: 500
                     placeholder: BLURRED
                     formats: [AUTO, WEBP, AVIF]
                 )
             }
         }
-        landscapeImage: file(relativePath: {eq: "landscape_image.png"} ){
+        landscapeImage: file(relativePath: {eq: "landscape_image.png"}){
             childImageSharp {
-                fluid(maxWidth: 500){
-                    ...GatsbyImageSharpFluid_noBase64
-                }
+                gatsbyImageData(
+                    width: 500
+                    placeholder: BLURRED
+                    formats: [AUTO, WEBP, AVIF]
+                )
             }
         }
-        paintingImage: file(relativePath: {eq: "painting_image.png"} ){
+        paintingImage: file(relativePath: {eq: "painting_image.png"}){
             childImageSharp {
-                fluid(maxWidth: 500){
-                    ...GatsbyImageSharpFluid_noBase64
-                }
+                gatsbyImageData(
+                    width: 500
+                    placeholder: BLURRED
+                    formats: [AUTO, WEBP, AVIF]
+                )
             }
         }
-        plumbingImage: file(relativePath: {eq: "plumbing_image.png"} ){
+        plumbingImage: file(relativePath: {eq: "plumbing_image.png"}){
             childImageSharp {
-                fluid(maxWidth: 500){
-                    ...GatsbyImageSharpFluid_noBase64
-                }
+                gatsbyImageData(
+                    width: 500
+                    placeholder: BLURRED
+                    formats: [AUTO, WEBP, AVIF]
+                )
             }
         }
-        weldingImage: file(relativePath: {eq: "welding_image.png"} ){
+        weldingImage: file(relativePath: {eq: "welding_image.png"}){
             childImageSharp {
-                fluid(maxWidth: 500){
-                    ...GatsbyImageSharpFluid_noBase64
-                }
+                gatsbyImageData(
+                    width: 500
+                    placeholder: BLURRED
+                    formats: [AUTO, WEBP, AVIF]
+                )
             }
         }
-        gardenImage: file(relativePath: {eq: "garden_image.png"} ){
+        gardenImage: file(relativePath: {eq: "garden_image.png"}){
             childImageSharp {
-                fluid(maxWidth: 500){
-                    ...GatsbyImageSharpFluid_noBase64
-                }
+                gatsbyImageData(
+                    width: 500
+                    placeholder: BLURRED
+                    formats: [AUTO, WEBP, AVIF]
+                )
             }
         }
-        electricalImage: file(relativePath: {eq: "electrical_image.png"} ){
+        electricalImage: file(relativePath: {eq: "electrical_image.png"}){
             childImageSharp {
-                fluid(maxWidth: 500){
-                    ...GatsbyImageSharpFluid_noBase64
-                }
+                gatsbyImageData(
+                    width: 500
+                    placeholder: BLURRED
+                    formats: [AUTO, WEBP, AVIF]
+                )
             }
         }
-        closetImage: file(relativePath: {eq: "closet_image.png"} ){
+        closetImage: file(relativePath: {eq: "closet_image.png"}){
             childImageSharp {
-                fluid(maxWidth: 500){
-                    ...GatsbyImageSharpFluid_noBase64
-                }
+                gatsbyImageData(
+                    width: 500
+                    placeholder: BLURRED
+                    formats: [AUTO, WEBP, AVIF]
+                )
             }
         }
-        applianceImage: file(relativePath: {eq: "appliance_image.png"} ){
+        applianceImage: file(relativePath: {eq: "appliance_image.png"}){
             childImageSharp {
-                fluid(maxWidth: 500){
-                    ...GatsbyImageSharpFluid_noBase64
-                }
+                gatsbyImageData(
+                    width: 500
+                    placeholder: BLURRED
+                    formats: [AUTO, WEBP, AVIF]
+                )
             }
         }
     }
